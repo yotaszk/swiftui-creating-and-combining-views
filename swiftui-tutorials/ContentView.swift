dcfd71d012e0bfc44be1695e9cf79c83e69c17f1
Yota Suzuki <apple.youta1214@gmail.com>
2019-10-11 17:30:39 +0900

    Initial Commit

---
+++ swiftui-tutorials/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-tutorials
+//
+//  Created by suzuki_y on 2019/10/11.
+//  Copyright © 2019 Yota Suzuki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui-tutorialsTests/swiftui_tutorialsTests.swift
@@ -0,0 +1,34 @@
+//
+//  swiftui_tutorialsTests.swift
+//  swiftui-tutorialsTests
+//
+//  Created by suzuki_y on 2019/10/11.
+//  Copyright © 2019 Yota Suzuki. All rights reserved.
+//
+
+import XCTest
+@testable import swiftui_tutorials
+
+class swiftui_tutorialsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
