4cd937b6cf3432bffb111a33a364ea6b5913167b
Yota Suzuki <apple.youta1214@gmail.com>
2019-10-12 08:19:34 +0900

    section 4

---
+++ swiftui-tutorials/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  swiftui-tutorials
+//
+//  Created by suzuki_y on 2019/10/12.
+//  Copyright © 2019 Yota Suzuki. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color .gray, lineWidth: 4))
+        .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
